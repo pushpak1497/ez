1a8a8ed8a570ae854b6201b374d0a127c04f7f06
pushpak1497 <150467635+pushpak1497@users.noreply.github.com>
2024-11-18 10:42:19 +0530

    changed db

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
 
 
-client = MongoClient("mongodb+srv://pushpakvyas1497:<db_password>@ezcluster0.kfsuv.mongodb.net")
+client = MongoClient("mongodb+srv://pushpakvyas1497:Pu$hpak1497@ezcluster0.kfsuv.mongodb.net")
 db = client.file_sharing_db
